3e5309333527749ac65dce84723eb0b040556d42
Elmagonza <elmagonz4.dev@gmail.com>
2023-11-09 18:58:57 -0300

    funcion de botones tarjeta

--- toda-lista/script.js
+++ toda-lista/script.js
@@ -2,39 +2,68 @@ const fecha = document.getElementById("fecha")
 const lista = document.getElementById("lista")
 const input = document.getElementById("input")
 const botonEnter = document.getElementById("enter")
+const check = "fa-check-circle"
+const uncheck ="fa-circle"
+const lineThrough ="line-through"
+let id = 0
 
 //FUNCION AGREGAR TAREA
 
 
 
-function agregarTarea (tarea){
+function agregarTarea (tarea,id,realizado,eliminado){
+
+    if(eliminado){return}
+    
+    const REALIZADO = realizado ? check : uncheck
+    const LINE = realizado ? lineThrough : ""
+
     const elemento =
         `<li class="elemento">
-            <i id="0" class="far fa-circle co" data="realizado"></i>
-            <p class="text ">${tarea}</p>
-            <i id="0"class="fas fa-trash de" data="eliminado"></i>
+            <i id="${id}" class="far ${REALIZADO}" data="realizado"></i>
+            <p class="text ${LINE}">${tarea}</p>
+            <i id="${id}" class="fas fa-trash de" data="eliminado"></i>
         </li>
         `
     lista.insertAdjacentHTML("beforeend", elemento)
 }
 
-// Convertir la primera letra a mayúscula y el resto a minúscula
-//tarea = 
+function tareaRealizada(element){
+    element.classList.toogle(check)
+    element.classList.toogle(uncheck)
+}
+
+
+
+
 
 botonEnter.addEventListener('click',()=>{
     const tarea = input.value
     if(tarea){
-       agregarTarea(tarea.charAt(0).toUpperCase() + tarea.slice(1).toLowerCase())
+       agregarTarea(tarea.charAt(0).toUpperCase() + tarea.slice(1).toLowerCase(),id,false,false)
     }
     input.value=""
+    id++
 })
 
 document.addEventListener('keyup',function(event){
     if(event.key == 'Enter'){
         const tarea = input.value
         if(tarea){
-        agregarTarea(tarea.charAt(0).toUpperCase() + tarea.slice(1).toLowerCase())
+        agregarTarea(tarea.charAt(0).toUpperCase() + tarea.slice(1).toLowerCase(),id,false,false)
     }
     input.value=""
     }  
+    id++
+})
+
+lista.addEventListener('click',function(event){
+    const element = event.target
+    const elementData = element.attributes.data.value
+    if (elementData==='realizado'){
+        tareaRealizada(element)
+    }
+    else if (elementData==='eliminado'){
+        tareaEliminada(element)
+    }
 })
